e0418caafff31ee7761a959b2ebd32a01fb1c9a8
Gary Rutland <gary.rutland@riseproject.com>
2017-11-14 10:00:27 +0000

    Updated copyright

--- js/global.js
+++ js/global.js
@@ -1,7 +1,6 @@
-var Global = new function () {
-  var _self = this;
-
+const Global = new function () {
   this.init = function () {
+    $('.copyright').find('.year').html((new Date()).getFullYear());
     $(function () {
       $("[data-toggle='tooltip']").tooltip();
       $("[data-toggle='popover']").popover();
